eba96ccebeb2561548727be900f0478d612ddd7b
aminemoh <alm.amine@gmail.com>
2018-10-27 09:47:38 +0100

    repos

--- pharmalina/ClientApp/src/app/models/repository.ts
+++ pharmalina/ClientApp/src/app/models/repository.ts
@@ -1,9 +1,17 @@
 import { Produit } from "./produit.model";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
 
+@Injectable()
 export class Repository {
-    constructor() {
-        this.produit = JSON.parse(document.getElementById("data").textContent);
-    }
+    constructor(private http: Http) {
+        this.getProduct(1);
+        }
+        getProduct(id: number) {
+        this.http.get("/api/produits/" + id)
+        .subscribe(response => this.produit = response.json());
+        }
 
     produit: Produit;
 }
+
